70b7380041ac561fbb60f3deef2885d96c380777
alzca1 <alzca1@gmail.com>
2021-03-02 19:15:16 +0100

    añadí un comentario al ejercicio

---
+++ Ejercicio1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace O3_Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Introduce el primer operando: ");
+            object respuesta = Console.Read();
+            // este es un comentario de prueba. 
+            
+        }
+    }
+}
+
